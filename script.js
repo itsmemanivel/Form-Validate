3a993302eab7396d68dc3cc2668319c9ccf4d043
karthick <karthickfeb5@gmail.com>
2021-04-23 08:06:37 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,144 @@
+
+    function csk()
+     {
+       var username=document.getElementById("username").value;
+       var password=document.getElementById("password").value;
+       if(username=="")
+       {
+           alert("Fill The CSK Captain Name");
+           return false;
+       }      
+      
+      else if(password=="")
+      {
+          alert("Fill The CSK color");
+          return false;
+      }
+    }
+
+    function rcb()
+    {
+      var Captainname=document.getElementById("Captainname").value;
+      var Color=document.getElementById("Color").value;
+      if(Captainname=="")
+      {
+          alert("Fill The RCB Captain Name");
+          return false;
+      }      
+     
+     else if(Color=="")
+     {
+         alert("Fill The RCB color");
+         return false;
+     }
+    }
+
+
+
+    function mi()
+     {
+       var mname=document.getElementById("mname").value;
+       var mcolor=document.getElementById("mcolor").value;
+       if(mname=="")
+       {
+           alert("Fill The MI Captain Name");
+           return false;
+       }      
+      
+      else if(mcolor=="")
+      {
+          alert("Fill The MI color");
+          return false;
+      }
+    }
+
+
+    function dc()
+     {
+       var dname=document.getElementById("dname").value;
+       var dcolor=document.getElementById("dcolor").value;
+       if(dname=="")
+       {
+           alert("Fill The DC Captain Name");
+           return false;
+       }      
+      
+      else if(dcolor=="")
+      {
+          alert("Fill The DC color");
+          return false;
+      }
+    }
+
+
+    function kkr()
+     {
+       var kname=document.getElementById("kname").value;
+       var kcolor=document.getElementById("kcolor").value;
+       if(kname=="")
+       {
+           alert("Fill The KKR Captain Name");
+           return false;
+       }      
+      
+      else if(kcolor=="")
+      {
+          alert("Fill The KKR color");
+          return false;
+      }
+    }
+
+
+    function pbks()
+     {
+       var pname=document.getElementById("pname").value;
+       var pcolor=document.getElementById("pcolor").value;
+       if(pname=="")
+       {
+           alert("Fill The PBKS Captain Name");
+           return false;
+       }      
+      
+      else if(pcolor=="")
+      {
+          alert("Fill The PBKS color");
+          return false;
+      }
+    }
+
+
+    function rr()
+     {
+       var rname=document.getElementById("rname").value;
+       var rcolor=document.getElementById("rcolor").value;
+       if(rname=="")
+       {
+           alert("Fill The RR Captain Name");
+           return false;
+       }      
+      
+      else if(rcolor=="")
+      {
+          alert("Fill The RR color");
+          return false;
+      }
+    }
+
+
+    function srh()
+     {
+       var sname=document.getElementById("sname").value;
+       var scolor=document.getElementById("scolor").value;
+       if(sname=="")
+       {
+           alert("Fill The SRH Captain Name");
+           return false;
+       }      
+      
+      else if(scolor=="")
+      {
+          alert("Fill The SRH color");
+          return false;
+      }
+    }
+
